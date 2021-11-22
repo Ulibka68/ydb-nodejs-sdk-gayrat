a3baa128f358beb1fa741d487883d31430f1904f
Ulibka68 <gena68@inbox.ru>
2021-11-22 15:09:18 +0300

    scheme-client

--- examples/scheme-client/index.ts
+++ examples/scheme-client/index.ts
@@ -1,31 +1,19 @@
 process.env.YDB_SDK_PRETTY_LOGS = '1';
 
-import {Driver, getCredentialsFromEnv, Logger} from 'ydb-sdk';
-import {main} from '../utils';
+import { initYDBdriver, driver, logger } from '../type-utils/ydb-functions';
 
-
-async function run(logger: Logger, entryPoint: string, dbName: string): Promise<void> {
-    const authService = getCredentialsFromEnv(entryPoint, dbName, logger);
-    logger.info('Driver initializing...');
-    const driver = new Driver(entryPoint, dbName, authService);
-    const timeout = 10000;
-    if (!await driver.ready(timeout)) {
-        logger.fatal(`Driver has not become ready in ${timeout}ms!`);
-        process.exit(1);
-    }
-
+async function run(): Promise<void> {
     logger.info('Testing scheme client capabilities...');
     await driver.schemeClient.makeDirectory('example-path');
     await driver.schemeClient.makeDirectory('example-path/subpath');
-    await driver.schemeClient.modifyPermissions(
-        'example-path/subpath',
-        [{
+    await driver.schemeClient.modifyPermissions('example-path/subpath', [
+        {
             grant: {
                 subject: 'tsufiev@staff',
-                permissionNames: ['read', 'use']
-            }
-        }]
-    );
+                permissionNames: ['read', 'use'],
+            },
+        },
+    ]);
     const entry = await driver.schemeClient.describePath('example-path');
     const children = await driver.schemeClient.listDirectory('example-path');
     logger.info(`Created path: ${JSON.stringify(entry, null, 2)}`);
@@ -34,4 +22,7 @@ async function run(logger: Logger, entryPoint: string, dbName: string): Promise<
     await driver.schemeClient.removeDirectory('example-path');
 }
 
-main(run);
+(async () => {
+    await initYDBdriver();
+    await run();
+})();
